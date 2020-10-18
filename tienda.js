608286660f5a6d2c1d2abd3c260af557b4c57775
IvoJack <72528052+IvoJack@users.noreply.github.com>
2020-10-18 10:45:40 -0500

    actualizacion

--- tienda.js
+++ tienda.js
@@ -3,86 +3,117 @@ setTimeout(function() {
 },8500);
 
 
-const addToShoppingCartButtons = document.querySelectorAll('.addToCart');
-addToShoppingCartButtons.forEach((addToCartButton) => {
-  addToCartButton.addEventListener('click', addToCartClicked);
+
+
+
+const escucharclic = document.querySelectorAll('.plus');
+
+escucharclic.forEach((clickplus) => {
+
+  clickplus.addEventListener('click', addToCartClicked);
+
+  
 });
 
+ 
 const comprarButton = document.querySelector('.comprarButton');
 comprarButton.addEventListener('click', comprarButtonClicked);
 
 const shoppingCartItemsContainer = document.querySelector(
   '.shoppingCartItemsContainer'
+ 
 );
 
 
 
-
-
 function addToCartClicked(event) {
+
   const button = event.target;
   const item = button.closest('.item');
-
   const itemTitle = item.querySelector('.item-title').textContent;
   const itemPrice = item.querySelector('.item-price').textContent;
   const itemImage = item.querySelector('.item-image').src;
   const itemCantidad = item.querySelector('.item_cantidad').value;
-// console.log(itemTitle, itemPrice, itemImage, itemCantidad)
-  
+  const itemtotal = item.querySelector('.item_total');
 
-  addItemToShoppingCart(itemTitle, itemPrice, itemImage, itemCantidad);
+  addItemToShoppingCart(itemTitle, itemPrice, itemImage, itemCantidad, itemtotal);
 }
 
-function addItemToShoppingCart(itemTitle, itemPrice, itemImage, itemCantidad) {
-  const elementsTitle = shoppingCartItemsContainer.getElementsByClassName(
+
+
+function addItemToShoppingCart(itemTitle, itemPrice, itemImage, itemCantidad, itemtotal) {
+  
+
+  const multptotal=0;
+  const totalppush = document.querySelector('.itemtotal')
+  const elementsTitle = shoppingCartItemsContainer.getElementsByClassName( 
     'shoppingCartItemTitle'
   );
-  for (let i = 0; i < elementsTitle.length; i++) {
+
+  for (let i = 0; i < elementsTitle.length; i++) { 
     if (elementsTitle[i].innerText === itemTitle) {
-      let elementQuantity = elementsTitle[
-        i
-      ].parentElement.parentElement.parentElement.querySelector(
+      let elementQuantity = elementsTitle[i].parentElement.parentElement.parentElement.querySelector(
         '.shoppingCartItemCantidad'
+        
       );
+     
+
       elementQuantity.value++;
-      $('.toast').toast('show');
       updateShoppingCartTotal();
-    //   return;
+      return;
+
+      
+ 
     }
+
   }
 
+
+  
   const shoppingCartRow = document.createElement('div');
+
   const shoppingCartContent= `
-  <div class="row3 shoppingCartItem">
-        <div class="col-6">
-            <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
+  <div class="row3 shoppingCartItem" id="shop1">
+        <span>
+        <div class="shopping-cart-item" >
                 <img class="imgpedido" src=${itemImage} class="shopping-cart-image  ">
-                <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0 itemtitulowa">${itemTitle}</h6>
-            </div>
-        </div>
-        <div class="col-2">
-            <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-                <p class="item-price mb-0 shoppingCartItemPrice itempricewa">${itemPrice}</p>
-            </div>
-        </div>
-        <div class="col-4">
-            <div
-                class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
-                <input class="item_cantidad shoppingCartItemCantidad itemcantidadwa" value="${itemCantidad}" type="number"
-                    >
-                <button class="btn btn-danger buttonDelete" type="button">X</button>
-            </div>
-        </div>
+                <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0  " id="itemtitulowa">${itemTitle}</h6>
+                </div>
+        </span>
+        <span>
+                <p class="item-price mb-0 shoppingCartItemPrice itempricewa" >${itemPrice}</p>
+        </span>
+      
+
+        <div class="item_cantidadt">
+   
+        <button onclick="this.parentNode.querySelector('input[type=number]').stepDown()" ></button>
+        <input class="quantity item_cantidad  shoppingCartItemCantidad " type="number" name="" min="0" value="${itemCantidad}" id="preciod">
+        <button onclick="this.parentNode.querySelector('input[type=number]').stepUp()" class="plus"></button>
+      </div>
+
+
+   
+
+            <span class="xcerrar">
+                <p class="item-total mb-0 shoppingCartItemTotal itemtotal" >${itemCantidad* Number(itemPrice.replace('$',''))}</p>
+                <button id="btn-shop" class="btn btn-danger buttonDelete" type="button"> x </button>
+            </span>
+
+
     </div>`;
+
+
+
+
   shoppingCartRow.innerHTML = shoppingCartContent;
 
-
   shoppingCartItemsContainer.append(shoppingCartRow);
-
-  shoppingCartRow
+  shoppingCartRow 
 
     .querySelector('.buttonDelete')
     .addEventListener('click', removeShoppingCartItem);
+    
     
 
   shoppingCartRow
@@ -90,200 +121,158 @@ function addItemToShoppingCart(itemTitle, itemPrice, itemImage, itemCantidad) {
     .addEventListener('change', quantityChanged);
 
 
-    // const changeitemcantidad = 
 
   updateShoppingCartTotal();
 }
 
-function itemchangedcantidad(event){
-    
-
-    // const changeitemcantidad =
-    // shoppingCartRow2
-    // .querySelector('.item-cantidad')
-    // .addEventListener('change', quantityChanged);
-
-    
-        // const input = event.target;
-        // input.value <= 0 ? (input.value = 1) : null;
-        // updateShoppingCartTotal();
-
-      
-      
-
- 
-}
-
-// function addwa(){
-// console.log("addwa ->  addwa",  addwa)
-
-//   let text = '';
 
 
+  function ertd(){
 
-//   const shoppingCarAddwa = document.querySelector(
-//     '.shoppingCartItemsContainer'
-//   );
+var leteryu = 
+    document.getElementById('cardnegocioid').innerHTML =  shoppingCartItemsContainer.childElementCount;
+
+   
+
+  }
+
+
   
-//   shoppingCarAddwa.forEach(element => {
-//     const buscarsolotext = document.querySelector('.row3')
-
-//     const row3 = $(
-//       shoppingCarAddwa.textContent.replace('$', '')
-//     );
-  
-//     text = text + row3;
-
-
-//   });
-
-// shoppingCartTotal.innerHTML =`${total.toFixed(2)}$`
-
-
-// }
-
-
 
 function updateShoppingCartTotal() {
   let total = 0;
+  let productoslist=""
   const shoppingCartTotal = document.querySelector('.shoppingCartTotal');
 
   const shoppingCartItems = document.querySelectorAll('.shoppingCartItem');
 
+  var pushlistadeproducots=document.querySelector('.fantasma');
 
 
   shoppingCartItems.forEach((shoppingCartItem) => {
+
+    const shopintitle=shoppingCartItem.querySelector(
+      '.shoppingCartItemTitle'
+    ).textContent;
+
 
 
     const shoppingCartItemPriceElement = shoppingCartItem.querySelector(
       '.shoppingCartItemPrice'
     );
+   
     const shoppingCartItemPrice = Number(
       shoppingCartItemPriceElement.textContent.replace('$', '')
     );
 
-
-
     const shoppingCartItemQuantityElement = shoppingCartItem.querySelector(
       '.shoppingCartItemCantidad'
-      
     );
-    
-
+  
     const shoppingCartItemCantidad = Number(
         shoppingCartItemQuantityElement.value
       );
-    //   console.log(shoppingCartItemCantidad)
+      //console.log(shoppingCartItemCantidad)
 
-
-    const shoppingCartItemQuantity = Number(
-      shoppingCartItemQuantityElement.value
-    );
+     const shoppingCartItemQuantity = Number(
+       shoppingCartItemQuantityElement.value
+     );
     total = total + shoppingCartItemPrice * shoppingCartItemCantidad;
+    
+
+    productoslist=productoslist+shoppingCartItemCantidad+"%20"+"*x*"+" | "+shopintitle+" | "+"precio"+"%20"+shoppingCartItemPrice+" | "+"total:"+"%20"+"%0A";
+  
+   
   });
-  shoppingCartTotal.innerHTML =`${total.toFixed(2)}$`
+  shoppingCartTotal.innerHTML =`$${total.toFixed(2)}`;
+  pushlistadeproducots.innerHTML=`${productoslist}`;
+  ertd();
+  
 }
+
+
+
+
 
 function removeShoppingCartItem(event) {
   const buttonClicked = event.target;
   buttonClicked.closest('.shoppingCartItem').remove();
   updateShoppingCartTotal();
+ 
 }
 
 function quantityChanged(event) {
   const input = event.target;
   input.value <= 0 ? (input.value = 1) : null;
+
   updateShoppingCartTotal();
+ 
 }
+
+
 
 function comprarButtonClicked() {
   shoppingCartItemsContainer.innerHTML = '';
   updateShoppingCartTotal();
+ 
+
 }
 
-// function ocultaritem (){
-//     var ocultaritem = document.addEventListener('click', function ocultar(){ $(".item_cantidad").fadeOut(3000)});
-
-
-    
-// }
 
 
 
-
-
-// setTimeout(function() {
-//          $(".unowhat").fadeOut(3000);
-//  },19500);
-
-
-// function resetear()
-// {
-//   var resetear = document.getElementById("myform").reset();
-  
-//   document.getElementById("demo").innerHTML =
-//   "";
-//   document.getElementById("divfantasma2");
-//   divfantasma2.style.display='none';divfantasma2.style.transition='0.5s linear';
-  
-
-
-// }
-
-// console.log(itemTitle, itemPrice, itemImage, itemCantidad)
+$("#ordernumber").val(function(){
+  return Math.floor(Math.random() * 999) + 1000
+})
 
 
 
- 
+
 
 
 function goToPage(){
+  
 
 
-
-
-
-
-//  console.log(itemTitle, itemPrice, itemImage, itemCantidad)
-// const item = button.closest('.item');
-
-//   const itemTitle = item.querySelector('.item-title').textContent;
-//   const itemPrice = item.querySelector('.item-price').textContent;
-
-//   const itemImage = item.querySelector('.item-image').src;
-//   const itemCantidad = item.querySelector('.item_cantidad').value;
-
-  var nombre = document.getElementById('entry.1').value;
+var orden = document.getElementById('ordernumber').value;  
+var nombre = document.getElementById('entry.1').value;
   var email = document.getElementById('entry.2').value;
   var whatsapp = document.getElementById('entry.3').value;
   var delivery = document.getElementById('entry.4').value;
   var direccion = document.getElementById('entry.5').value;
-
-  // var producto1 = document.getElementsByClassName('itemtitulowa').textContent;
   
 
-  // const itemPrice = item.querySelector('.item-price').textContent;
-  // const itemImage = item.querySelector('.item-image').src;
-  // const itemCantidad = item.querySelector('.item_cantidad').value;
-  // var precio1 = document.querySelector('.1.99$').textContent;
-  // var cantidad1 = document.querySelector('.1').value;
-
-  // var producto2 = document.querySelector('.Alitas').textContent;
-  // var precio2 = document.querySelector('.5.99$').textContent;
-  // var cantidad2 = document.querySelector('.1').value;
-
-  // var producto3 = document.querySelector('.Papas').textContent;
-  // var precio3 = document.querySelector('.1.99$').textContent;
-  // var cantidad3 = document.querySelector('.1').value;
+  var totalf = document.getElementById('totalwa').textContent;
 
 
-  // var producto4 = document.querySelector('.Hamburguesa').textContent;
-  // var precio4 = document.querySelector('.2.99$').textContent;
-  // var cantidad4 = document.querySelector('.1').value;
+  var totalfp = Number(
+    totalf.replace('$', '')
+  );if( totalfp <= 10|| /^\s+$/.test(totalfp) )  {
+    return (document.getElementById("demo").innerHTML =
+    "Tu pedido es menor $10.00")
+  
+}
+
+
+var radios = document.getElementsByName('ok2');
+
+
+for (var i = 0, length = radios.length; i < length; i++) {
+  if (radios[i].checked) {
+ 
+    var retornapagos = (radios[i].value);
 
  
 
+  }
+}
+
+
   
+
+var mostrarpedidof=document.querySelector('.fantasma').textContent;
+
+
   if( nombre == null || nombre.length == 0 || /^\s+$/.test(nombre) ) {
      
     return (document.getElementById("demo").innerHTML =
@@ -309,11 +298,75 @@ function goToPage(){
     return (document.getElementById("demo").innerHTML =
     "Ingresa tu dirección con con referencia")
   }
+
+
+
+
+
+
+  
   
 
-var myWindow = window.open ("https://midominio.com/"+"Demo Ecommerce BurgerKings" + "%20" + "%0A"+"✅ _Pedido confirmado_" + "%0A"+"_________________________" + "%0A"+"%0A"+
- "*Nombre*" +"%0A" + nombre + "%20" +"%0A"+"%0A" +"*Email*" +"%0A" + email +  "%20" + "%0A"+"%0A"+"*Costo Delivery*"+"%0A" + delivery + "%20" +"%0A"+ "%0A"+"*Direcciòn con referencia*"+"%0A" +direccion+"%0A"+ "%0A"+"%0A"+ "%0A"
-+"_________________________" + "%0A" + "%0A"+ "Su pedido es:"  + "%0A" + "%0A");
+// var myWindow = window.open ("https://midominio.com/"+"Demo Ecommerce BurgerKings" + "%20" + "*WhatsBulkSender™*" + "%0A"+"✅  _Pedido confirmado_" + "Orden N°"+orden + "%0A"+"_________________________" + "%0A"+"%0A"+
+//  "*Nombre*" +"%0A" + nombre + "%20" +"%0A"+"%0A" +"*Email*" +"%0A" + email +  "%20" + "%0A"+"%0A"+"*Costo Delivery*"+"%0A" + delivery + "%20" +"%0A"+ "%0A"+"*Dirección con referencia*"+"%0A" +direccion+"%0A"+totalf+ pedidof+ "%0A"+"%0A"+
+//   "%0A"+"%0A"+ "%0A" +"_________________________" + "%0A" + "%0A"+"*P.D.*"+ "%20"+ "Guarda nuestro contacto como  para recibir 15 minutos antes el link de Zoom. Empezamos puntual 😉👍");
+
+
+
+var myWindow = window.open("https://midominio.com/"+"*Demo Ecommerce FastFood*" + "%20" + "*WhatsBulkSender™*" + "%0A"+"✅ *Pedido confirmado*"+"%0A"+"_Orden N°_"+ "%20"+"---"+"%20"+orden+"%20"+"---"+ "%0A"+"_________________________" + "%0A"+"%0A"+"*Tu pedido es:*"+ "%0A"+ "%0A"+mostrarpedidof+ "%0A"+ "%0A"+"*Total:*"+ "%20"+totalfp+ "%0A"+"_________________________"+ "%0A"+"%0A"+
+ "*Nombre:*" +"%20"+ nombre+"%0A"+"*Email:*" +"%20" + email+"%0A"+"*Costo Delivery:*"+"%20" + delivery+"%0A"+"*Dirección con referencia:*"+"%20" +direccion+"%0A" +"_________________________"  + "%0A"+"%0A"+"*Forma de pago:*"+"%0A"+ retornapagos+"%0A"+"%0A"+"*Total:*"+ "%20"+totalfp+"%0A"+"%0A"+ "_Para ver más demos ingresa al siguiente link:_"+"%0A");
+
 
 
 }
+
+$("#boton").on("click",function(event){
+  event.preventDefault();
+
+});
+
+
+
+// animacion formas de pago 
+
+$('.eti1').on('click', function(){
+  $('.datos1').addClass('show btnpagos');
+ 
+  $('.datos2').removeClass('show btnpagos');
+  $('.datos3').removeClass('show btnpagos');
+  $('.datos4').removeClass('show btnpagos');
+
+})
+
+
+$('.eti2').on('click', function(){
+  $('.datos2').addClass('show btnpagos');
+  $('.datos1').removeClass('show btnpagos');
+  $('.datos3').removeClass('show btnpagos');
+  $('.datos4').removeClass('show btnpagos');
+})
+
+$('.eti3').on('click', function(){
+  $('.datos3').addClass('show btnpagos');
+  $('.datos1').removeClass('show btnpagos');
+  $('.datos2').removeClass('show btnpagos');
+  $('.datos4').removeClass('show btnpagos');
+})
+
+$('.eti4').on('click', function(){
+  $('.datos4').addClass('show btnpagos');
+  $('.datos1').removeClass('show btnpagos');
+  $('.datos2').removeClass('show btnpagos');
+  $('.datos3').removeClass('show btnpagos');
+})
+
+// stylos input
+
+$('.btn-plus, .btn-minus').on('click', function(e) {
+  const isNegative = $(e.target).closest('.btn-minus').is('.btn-minus');
+  const input = $(e.target).closest('.input-group').find('input');
+  if (input.is('input')) {
+    input[0][isNegative ? 'stepDown' : 'stepUp']()
+  }
+})
+
